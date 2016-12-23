b51edb32f3ef9d2b4b16a11de01b65936f59873f
Thiago Bueno Fiorenza <thiagobfiorenza@gmail.com>
2016-12-23 11:16:34 -0200

    fix login user image

--- client/templates/sidebars.js
+++ client/templates/sidebars.js
@@ -2,14 +2,6 @@ import {Template} from 'meteor/templating';
 import {Meteor} from 'meteor/meteor';
 
 Template.sidebar.helpers({
-    emailAddress: function () {
-        var user = Meteor.user();
-        if (user && user.emails) {
-            return user.emails[0].address;
-        } else {
-            return "";
-        }
-    },
     userId: function () {
         var user = Meteor.user();
         if (user && user._id) {
@@ -17,5 +9,33 @@ Template.sidebar.helpers({
         } else {
             return "";
         }
+    },
+    userName: function () {
+        var user = Meteor.user();
+        if (user && user.services.facebook) {
+            return user.services.facebook.name;
+        }
+        else {
+            return "";
+        }
+    },
+    userEmail: function () {
+        var user = Meteor.user();
+        if (user && user.emails) {
+            return user.emails[0].address;
+        } else if (user && user.services.facebook) {
+            return user.services.facebook.email;
+        }
+        else {
+            return "";
+        }
     }
 });
+
+if (Meteor.isClient) {
+    Template.sidebar.events({
+        'click nav.sidebar li a': function(){
+            $('body').removeClass('aside-toggled');
+        }
+    });
+}
