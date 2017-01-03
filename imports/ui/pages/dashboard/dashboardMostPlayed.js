cb15b388f9d4f8b219f3c7f189e2b8a1ac4e8a01
João Gabriel <mailparajg@gmail.com>
2017-01-03 09:58:44 -0200

    Temporario de grafico de mais jogados

--- imports/ui/pages/dashboard/dashboardMostPlayed.js
+++ imports/ui/pages/dashboard/dashboardMostPlayed.js
@@ -1,28 +1,29 @@
-window.onload = function () {
-
-//Better to construct options first and then pass it as a parameter
-	var options = {
-		title: {
-			text: "Column Chart using jQuery Plugin"
-		},
-                animationEnabled: true,
-		data: [
-		{
-			type: "column", //change it to line, area, bar, pie, etc
-			dataPoints: [
-				{ x: 10, y: 10 },
-				{ x: 20, y: 11 },
-				{ x: 30, y: 14 },
-				{ x: 40, y: 16 },
-				{ x: 50, y: 19 },
-				{ x: 60, y: 15 },
-				{ x: 70, y: 12 },
-				{ x: 80, y: 10 }
-			]
-		}
-		]
-	};
-
-	$("#chartContainer").CanvasJSChart(options);
-
-}
+// var Highcharts = require('highcharts');
+//
+// if (Meteor.isClient) {
+//   Meteor.subscribe("Tasks");
+//
+//   Template.dashboardMostPlayed.helpers({
+//     createChart: function () {
+//       // Gather data:
+//       var allTasks = Tasks.find().count(),
+//             incompleteTask = Tasks.find({checked: {$ne: true}}).count(),
+//             tasksData = [{
+//                 y: incompleteTask,
+//                 name: "Incomplete"
+//              }, {
+//                  y: allTasks - incompleteTask,
+//                  name: "Complete"
+//              }];
+//       // Use Meteor.defer() to craete chart after DOM is ready:
+//       Meteor.defer(function() {
+//         // Create standard Highcharts chart with options:
+//         Highcharts.chart('chart', {
+//           series: [{
+//             type: 'pie',
+//             data: tasksData
+//           }]
+//         });
+//       });
+//     }
+//   });
