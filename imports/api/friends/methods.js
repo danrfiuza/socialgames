5ab6cd63b516f3c353c5f633ca9b069e58c4d85a
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2017-01-03 23:19:24 -0200

    Gato temporario para fazer funcionar o seguir um amigo

--- imports/api/friends/methods.js
+++ imports/api/friends/methods.js
@@ -18,3 +18,38 @@ Meteor.methods({
 
     }
 });
+
+/*******************************************************************
+** GATO TEMPORARIO ATÉ DESCOBRIR PQ O API/FRIENDS/FRIENDS.JS NÃO
+** ESTA SENDO IMPORTADO
+********************************************************************/
+//export const  Friends = new Mongo.Collection('friends');
+
+// Monta um registro de amigo vindo do facebook
+function buildFriendFacebook(dados, usuario) {
+    return friend = {
+        nome: usuario.profile.name,
+        email: usuario.services.facebook.email,
+        amigo_id: dados.amigos,
+        meu_id: Meteor.user()._id
+    }
+}
+
+// Monta um registro de amigo da base
+function buildFriendBase(dados, usuario) {
+    return friend = {
+        email: usuario.emails[0].address,
+        amigo_id: dados.amigos,
+        meu_id: Meteor.user()._id
+    }
+}
+
+// Verifica se o amigo já existe na collection de amigos
+function isFriendExist(dados) {
+    var qtdAmigo = Friends.find({amigo_id:dados.amigos, meu_id: Meteor.user()._id}).count();
+    if (qtdAmigo > 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
