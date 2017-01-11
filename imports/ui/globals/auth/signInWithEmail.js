7dfff475c08334ab40f47a820e8a182235bd4535
mailparajg@gmail.com <mailparajg@gmail.com>
2017-01-11 10:57:59 -0200

    Arrumado save do e-mail do usuario e adicionado localização

--- imports/ui/globals/auth/signInWithEmail.js
+++ imports/ui/globals/auth/signInWithEmail.js
@@ -1,22 +1,23 @@
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 import './signInWithEmail.html';
 
 Template.signInWithEmail.events({
-    "submit #signup-form": function(event, template) {
+    "submit #signup-form": function (event, template) {
         event.preventDefault();
 
         Accounts.createUser({
-            username: template.find("#signup-email").value,
+            email: template.find("#signup-email").value,
             password: template.find("#signup-password").value,
             profile: {
                 name: template.find("#signup-name").value
                 // Other required field values can go here
             }
-        }, function(error) {
+        }, function (error) {
             if (error) {
                 swal('Oops...', error.message, 'error');
-            }else{
-                swal('Registro efetuado com sucesso', '', 'success');
+            } else {
+                // swal('Registro efetuado com sucesso', '', 'success');
+                swal(TAPi18n.__('login.SUCCESSFUL_REGISTRATION'), '', 'success');
                 Router.go('login');
             }
         });
