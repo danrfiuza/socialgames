4902269df3983393bb3882d4911a1087dd144012
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2017-01-11 23:11:01 -0200

    Feita contagem de quantidade de games diferentes q o jogador já jogou

--- imports/api/match/methods.js
+++ imports/api/match/methods.js
@@ -13,25 +13,15 @@ Meteor.methods({
         return Matchs.find({"players.mail": user.emails[0].address}).count();
     },
     'matchs.findCountDistinct'(user){
-        console.log('server de verdade');
-        // console.log(user);
-        // var distinctEntries = _.uniq(Matchs.find({"players.mail": user.emails[0].address}, {
-        //     sort: {
-        //         "game._id": 1
-        //     }
-        // }).fetch(), true, doc => {
-        //     return doc.game._id;
-        // });
-
-
         result = Matchs.find({"players.mail": user.emails[0].address}, { sort: { "game._id": 1 } } ).fetch();
 
-        count = 0;
+        arrCount = [];
         result.forEach(function (value) {
-            console.log(value);
+            if (! _.contains( arrCount, value.game) ) {
+                arrCount.push(value.game);
+            }
         });
 
-        console.log(distinctEntries);
-        return distinctEntries.length;
+        return arrCount.length;
     }
 });
