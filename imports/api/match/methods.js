230f06c35066147dfe66438dd961b307aace1e61
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2017-01-14 19:29:02 -0200

    implementado grafico agora trazendo valores reais

--- imports/api/match/methods.js
+++ imports/api/match/methods.js
@@ -1,10 +1,10 @@
 import {Meteor} from 'meteor/meteor';
 import {Matchs} from './match.js';
-import { Games } from '../game/game.js';
+import {Games} from '../game/game.js';
 
 Meteor.methods({
     'matchs.insert'(match) {
-        result = Matchs.insert(match, function(err, result){
+        result = Matchs.insert(match, function (err, result) {
             if (err == null) {
                 return result;
             } else {
@@ -20,11 +20,11 @@ Meteor.methods({
         return Matchs.find({"players.mail": user.emails[0].address}).count();
     },
     'matchs.findCountDistinct'(user){
-        result = Matchs.find({"players.mail": user.emails[0].address}, { sort: { "game._id": 1 } } ).fetch();
+        result = Matchs.find({"players.mail": user.emails[0].address}, {sort: {"game._id": 1}}).fetch();
 
         arrCount = [];
         result.forEach(function (value) {
-            if (! _.contains( arrCount, value.game) ) {
+            if (!_.contains(arrCount, value.game)) {
                 arrCount.push(value.game);
             }
         });
@@ -32,42 +32,28 @@ Meteor.methods({
         return arrCount.length;
     },
     'matchs.gamesTop30'(){
-        console.log('chmou o chamado');
-        var data30atras = new  Date();
+        var data30atras = new Date();
         data30atras.setDate(data30atras.getDate() - 30);
         dateTimeStamp = data30atras.getTime();
 
-        // result = Matchs.find({ created_at: { $gt: dateTimeStamp }  }, {sort: {_id: -1} }).fetch();
-
-        var distinctEntries = _.uniq(Matchs.find({ created_at: { $gt: dateTimeStamp }  }, {
+        var distinctEntries = _.uniq(Matchs.find({created_at: {$gt: dateTimeStamp}}, {
             sort: {game: 1}, fields: {game: true}
-        }).fetch().map(function(x) {
+        }).fetch().map(function (x) {
             return x.game;
         }), true);
 
-        console.log('aki estamos');
         arrObj = [];
+        totalPartidas = 0;
         _.map(distinctEntries, function (value) {
-            console.log(Games.find({ _id: value}).fetch());
-            // arrObj.push();
+            prepareValue = Games.findOne({_id: value});
+            prepareValue.totalPartidas30dias = Matchs.find({game: value, created_at: {$gt: dateTimeStamp}}).count();
+            totalPartidas = totalPartidas + prepareValue.totalPartidas30dias;
+            arrObj.push(prepareValue);
         });
 
-        // console.log(Meteor.Games.find({}));
-
-
-
-
-
-        // arrCount = [];
-        // objDistinctGames = {};
-        // result.forEach(function (value) {
-        //     if (! _.contains( arrCount, value.game) ) {
-        //         arrCount.push(value.game);
-        //         objDistinctGames = { value.game: { matchs: 0 } };
-        //     }
-        // });
-
-
-        // return arrCount.length;
+        return {
+            totalPartidas: totalPartidas,
+            arrObj: arrObj
+        };
     }
 });
