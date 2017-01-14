d61648301e2f51b89f470b3e7a675ef6bb70d83e
Andre Amorim - Note <andre.php6@gmail.com>
2017-01-14 19:58:26 -0200

    Restructuring functionalities matches

--- imports/ui/pages/mach/match.js
+++ imports/ui/pages/mach/match.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { State } from './state.js';
+import { Service } from './service.js';
 import './match.html';
 
 Meteor.subscribe('game.list');
@@ -20,27 +22,27 @@ clock.stop();
 
 Template.matches.rendered = function(){
     $("#selectGame").select2({
-    	placeholder: TAPi18n.__('match.HINT_CHOOSE_GAME'),
-    	maximumSelectionLength: 1,
-      	allowClear: true
+        placeholder: TAPi18n.__('match.HINT_CHOOSE_GAME'),
+        maximumSelectionLength: 1,
+        allowClear: true
     });
     $("#place").select2({
         placeholder: "Selecione um local",
         maximumSelectionLength: 1,
         allowClear: true
     });
-    changeState('begin');
+    State.change('begin');
     rPlayers.set([]);
     rGame.set([]);
 };
 
 Template.matches.helpers({
-	games() {
+    games() {
         return Games.find();
-	},
-	game() {
-		return rGame.get();
-	},
+    },
+    game() {
+        return rGame.get();
+    },
     maxplayers() {
         return rMaxPlayers.get();
     },
@@ -81,22 +83,22 @@ Template.matches.helpers({
 Template.matches.events({
     // Search games
     'change #selectGame' : function(event, template) {
-        changeState('game');
-      	game = Games.findOne({bggid: $("#selectGame").val()[0]});
-		rGame.set(game);
+        State.change('game');
+        game = Games.findOne({bggid: $("#selectGame").val()[0]});
+        rGame.set(game);
         rMaxPlayers.set(game.maxplayers);
         rMinPlayers.set(game.minplayers);
         rPlayers.set([]); 
     },
     'click #btnMatchNow' : function(event, template) {
-        changeState('match-now');
-        loadComboFriends();
-        prepareAutoComplateForPlayers();
+        State.change('match-now');
+        Service.loadComboFriends(rComboFriends);
+        Service.prepareAutoComplateForPlayers(rMaxPlayers);
     },
     'click #btnScheduleMatch' : function(event, template) {
-        changeState('match-schedule');
-        loadComboFriends();
-        prepareAutoComplateForPlayers();
+        State.change('match-schedule');
+        Service.loadComboFriends(rComboFriends);
+        Service.prepareAutoComplateForPlayers(rMaxPlayers);
     },
     'change .selectPlayer' : function(event, template) {
         var selectedIndex = event.target.selectedIndex;
@@ -105,289 +107,51 @@ Template.matches.events({
             var imgPlayer = '<em class="fa fa-user fa-2x">';
             var emailPlayer = event.target.options[selectedIndex].innerHTML;
             $("#imgPlayer"+this.index).html(imgPlayer);
-            addPlayerMatch(emailPlayer);
-            setStatusItemComboFriends(selectedIndex, 'disabled');
-            prepareAutoComplateForPlayers();
+            Service.addPlayerMatch(emailPlayer, rPlayers);
+            Service.setStatusItemComboFriends(selectedIndex, 'disabled', rComboFriends);
+            Service.prepareAutoComplateForPlayers(rMaxPlayers);
         }
     },
     'select2:unselect .selectPlayer' : function(event, template) {
         var selectedIndex = event.target.selectedIndex;
         $("#imgPlayer"+this.index).html("");
-        removePlayerMatch(selectedIndex);
+        Service.removePlayerMatch(selectedIndex, rPlayers);
     },
     'click #btnStartMatch' : function(event, template) {
-        if (minPlayersFilled()) {
-            changeState('start');
+        if (Service.minPlayersFilled(rPlayers, rMinPlayers)) {
+            State.change('start');
             clock.start();
         } else {
             alert("Para este jogo deve ter no mínimo " + rMinPlayers.get() + ' players selecionados');
         }
     },
     'click #btnFirstPlayer' : function(event, template) {
-        randomizeFirstPlayer();
+        Service.randomizeFirstPlayer(rPlayers);
         $('#btnFirstPlayer').hide();
     },
     'click #btnFinishCount' : function(event, template) {
         clock.stop();
-        disableComboPlayers();
-        changeState('score');
+        Service.disableComboPlayers(rMaxPlayers);
+        State.change('score');
     },
     'click #btnFinishMatch' : function(event, template) {
-        if (isValidScore()) {
-            orderRanking();
-            changeState('trophy');
-            salveMatch(buildMatch());
+        if (Service.isValidScore(rMaxPlayers)) {
+            Service.orderRanking(rPlayers, rMaxPlayers, rPodium);
+            State.change('trophy');
+            Service.saveMatch(Service.buildMatch(rPodium, rGame, clock.elapsedTime()));
         } else {
             alert("Algo está errado com a pontuação informada");
         }
     },
     'click #btnSaveSchedule' : function(event, template) {
-        salveMatch(buildMatchSchedule());
-        changeState('schedule');
+        var matchId = Service.saveMatch(Service.buildMatchSchedule(rPlayers));
+        rMatchId.set(matchId);
+        State.change('schedule');
     },
     'click #btnPublishFacebook': function(event, template) {
-        printScreen($('#imgShareMatch').html());
+        Service.printScreen($('#imgShareMatch').html());
     },
     'click #btnNewMatch': function(event, template) {
         location.href = '/matches';
     }
 });
-
-// State machine of match
-function changeState(status) {
-    switch (status) {
-        case 'begin' :
-            $('#panelSearch').show();
-            $('#viewGame').hide();
-            $('#divPlayers').hide();
-            $('#divButtons').hide();
-            $('#divBtnStarMatch').hide();
-            $('#divBtnFirstPlayer').hide();
-            $('#divBtnFinishMatch').hide();
-            $('#classification').hide();
-            $('#divBtnPublish').hide();
-            $('#readyPlayers').hide();
-            $('#divBtnSaveSchedule').hide();
-            $('#divSchedule').hide();
-            $('#msgReservationFriend').hide();
-            $('#msgReservationSchedule').hide();
-            $('#divPlaces').hide();
-            $('#imgShareMatch').hide();
-            $('#divTimer').hide();
-            break;
-        case 'game' :
-            $('#panelSearch').hide();
-            $('#divSchedle').hide();
-            $('#viewGame').show();
-            $('#divButtons').show();
-            break;
-        case 'match-now' : 
-            $('#divButtons').hide();
-            $('#divPlayers').show();
-            $('#divBtnStarMatch').show();
-            $('#panelSearch').hide();
-            $('#divPlaces').show();
-            break;
-        case 'match-schedule' : 
-            $('#divButtons').hide();
-            $('#divPlayers').show();
-            $('#divBtnSaveSchedule').show();
-            $('#divSchedule').show();
-            $('#panelSearch').hide();
-            $('#msgReservationFriend').show();
-            $('#divPlaces').show();
-            break;
-        case 'start' : 
-            $('#divTimer').show();
-            $('#divBtnStarMatch').hide();
-            $('#divBtnFirstPlayer').show();
-            $('#panelSearch').hide();
-            $('#divPlayers').hide();
-            $('#readyPlayers').show();
-            $('#divPlaces').hide();
-            $('#subtitleGame').html("Partida em andamento");
-            $('#pBtnCountPoints').html('');
-            break;
-        case 'score' : 
-            $('#divPlayers').show();
-            $('#readyPlayers').hide();
-            $('#subtitleGame').html("Contagem de pontos");
-            $('#btnFinishCount').hide();
-            $('#divBtnFinishMatch').show();
-            break;
-        case 'trophy' : 
-            $('#panelSearch').hide();
-            $('#classification').show();
-            $('#divPlayers').hide();
-            $('#divBtnFinishMatch').hide();
-            $('#divBtnPublish').show();
-            break;
-        case 'schedule' : 
-            $('#divBtnSaveSchedule').hide();
-            $('#msgReservationSchedule').show();
-            break;
-    }
-}
-
-// Mount select2 in combo players elements 
-function prepareAutoComplateForPlayers() {
-    for (var i = 1; i <= rMaxPlayers.get(); i++) {
-        $("#player"+i).select2({
-            placeholder: "digite o email do seu amigo",
-            maximumSelectionLength: 1
-        });
-    }
-}
-
-// Load select combo with friends
-function loadComboFriends() {
-    friends = Meteor.users.findOne({_id: Meteor.user()._id}).profile.friends;
-    comboFriends = [];
-    var cont = 1;
-    var emailUserLogged = captureEmail(Meteor.user());
-    comboFriends[0] = { id : Meteor.user()._id, text : emailUserLogged}
-
-    _.forEach(friends, function(item){
-        emailAmigo = captureEmail(Meteor.users.findOne({_id: item.friend_id}));
-        comboFriends[cont] = { id : item.friend_id, text : emailAmigo };
-        cont++;
-    });
-
-    rComboFriends.set(comboFriends);
-}
-
-// caputure email from user
-function captureEmail(user) {
-    if (user.services.facebook) {
-        email = user.services.facebook.email;
-    } else {
-        email = user.emails[0].address;
-    }
-    return email;
-}
-
-// set status in option select friends ex. disabled
-function setStatusItemComboFriends(index, status) {
-    var arrFriends = rComboFriends.get();
-    arrFriends[index].status = status;
-    rComboFriends.set(arrFriends);
-}
-
-// Valid if min players is selected for match
-function minPlayersFilled() {
-    if (rPlayers.get().length < rMinPlayers.get()) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-// Remove a player of match
-function removePlayerMatch(index) {
-    var players = rPlayers.get();
-    players.splice(index, 1);
-    rPlayers.set(players);
-}
-
-// Include a player of match
-function addPlayerMatch(emailPlayer) {
-    var players = rPlayers.get();
-    players.push({ mail : emailPlayer, first : false });
-    rPlayers.set(players);
-}
-
-// Randomize the first player
-function randomizeFirstPlayer() {
-    var players = rPlayers.get();
-    var numFirst = Math.floor((Math.random() * players.length) + 1);
-    players[numFirst - 1].first = true;
-    rPlayers.set(players);
-}
-
-// Disable select list of players not used
-function disableComboPlayers() {
-    for (var i = 1; i <= rMaxPlayers.get(); i++) {
-        $("#player"+i).prop('disabled', 'disabled');
-    }
-}
-
-// Validates players' scores
-function isValidScore() {
-    var isValid = true;
-    for (var i = 1; i <= rMaxPlayers.get(); i++) {
-        if ( $("#player"+i).val().length != 0 ) {
-            if (! $.isNumeric($("#ptPlayer"+i).val()) ) {
-                isValid = false;
-            }
-        }
-    }
-    return isValid;
-}
-
-// Sort the players according to the score of the match
-function orderRanking() {
-    var players = rPlayers.get();
-    for (var i = 1; i <= rMaxPlayers.get(); i++) {
-        if ( $("#player"+i).length != 0 ) {
-            if (players[i - 1] != undefined) {
-                players[i-1].pontos = $("#ptPlayer"+i).val();
-            }
-        }
-    }
-    players.sort(function(a, b){
-        if (parseInt(a.pontos) > parseInt(b.pontos)) {
-            return -1;
-        } else if (parseInt(a.pontos) < parseInt(b.pontos)) {
-            return 1;
-        } else {
-            return 0; 
-        }
-    });
-    rPlayers.set(players);
-    rPodium.set(players);
-}
-
-function buildGenericMatch() {
-    var match = {};
-    match.game = rGame.get()._id;
-    if ($('#place').val().length > 0) {
-        match.place = $('#place').val()[0];
-    }
-    return match;
-}
-
-// Assemble match information
-function buildMatch() {
-    var match = buildGenericMatch();
-    match.players = rPodium.get();
-    match.timer = clock.elapsedTime();
-    match.created_at = new Date().getTime();
-    return match;
-}
-
-// Assemble match schedule information
-function buildMatchSchedule() {
-    var match = buildGenericMatch();
-    match.players = rPlayers.get();
-    match.date_schedule = $('#dateMatch').val();
-    match.created_at = new Date().getTime();
-    return match;
-}
-
-// Persists a match
-function salveMatch(match) {
-    Meteor.call('matchs.insert', match, function (e, result) {
-        if(result) {
-            console.log("Partida foi salva no banco de dados");
-            rMatchId.set(result);
-        } else {
-            console.log("Erro ao tentar salvar uma partida");
-        }
-    });
-}
-
-// Screeshot of trophy with match results
-function printScreen(html) {
-    var css = '';
-    Meteor.call('webshot.snap', {html:html, css:css}, function (e, result) {});
-}
