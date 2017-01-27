2d0bec5b2e74de11e170c90916cb5eb553fc18ab
Thiago Bueno Fiorenza <thiagobfiorenza@gmail.com>
2017-01-27 15:58:58 -0200

    update color of topbar in schedule matches

--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -1,24 +1,21 @@
-import { Template } from 'meteor/templating';
-import '../globals/app.init.js';
-import '../globals/custom.js';
-
-import '../components/angle/modules/bootstrap-start.js';
-import '../components/angle/modules/clear-storage.js';
-import '../components/angle/modules/constants.js';
-import '../components/angle/modules/localize.js';
-import '../components/angle/modules/navbar-search.js';
-import '../components/angle/modules/sidebar.js';
-import '../components/angle/modules/toggle-state.js';
-import '../components/angle/modules/utils.js';
-
-import '../components/avatar/avatar.js';
-
-import '../globals/footer.html';
-import '../globals/sidebar.html';
-import '../globals/submenu.html';
-import '../globals/topnavbar.html';
-import '../globals/offsidebar.html';
-import './app-body.html';
+import {Template} from "meteor/templating";
+import "../globals/app.init.js";
+import "../globals/custom.js";
+import "../components/angle/modules/bootstrap-start.js";
+import "../components/angle/modules/clear-storage.js";
+import "../components/angle/modules/constants.js";
+import "../components/angle/modules/localize.js";
+import "../components/angle/modules/navbar-search.js";
+import "../components/angle/modules/sidebar.js";
+import "../components/angle/modules/toggle-state.js";
+import "../components/angle/modules/utils.js";
+import "../components/avatar/avatar.js";
+import "../globals/footer.html";
+import "../globals/sidebar.html";
+import "../globals/submenu.html";
+import "../globals/topnavbar.html";
+import "../globals/offsidebar.html";
+import "./app-body.html";
 
 Template.layout.rendered = function () {
     setTimeout(function () {
@@ -28,12 +25,14 @@ Template.layout.rendered = function () {
 
 Template.layout.helpers({
     sectionClass: function () {
-    	var css = 'section-';
-    	var route = Router.current().route.options.path.replace('/', '');
-    	if (Meteor.userId()) {
-    		return 'wrapper ' + css + route;
-    	} else {
-    		return css + route;
-    	}
+        var css = 'section-';
+        console.log(Router);
+        console.log(Router.current().route.options);
+        var route = Router.current().route.options.path.split('/')[1];
+        if (Meteor.userId()) {
+            return 'wrapper ' + css + route;
+        } else {
+            return css + route;
+        }
     }
 });
