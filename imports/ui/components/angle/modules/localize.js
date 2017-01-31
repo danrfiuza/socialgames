a7663f01bead62bc055c4686a0c5167ebd1db131
andre <andre.php6@gmail.com>
2017-01-31 13:43:52 -0200

    fixed issue #44

--- imports/ui/components/angle/modules/localize.js
+++ imports/ui/components/angle/modules/localize.js
@@ -1,33 +1,30 @@
-// TRANSLATION
-// -----------------------------------
+(function (window, document, $, undefined) {
 
-(function(window, document, $, undefined) {
+    var preferredLang = 'pt';
+    var storageKey = 'jq-appLang';
 
-    window.initTranslation = function() {
-
-        var preferredLang = 'en';
-        var storageKey = 'jq-appLang';
+    App.ready(function () {
 
         // detect saved language or use default
-        var currLang = $.localStorage.get(storageKey) || preferredLang;
+        var userLanguage = localStorage.getItem('userLanguage') || window.navigator.userLanguage || window.navigator.language;
+        userLanguage = userLanguage.split("-");
+        userLanguage = userLanguage[0];
+
+        var currLang = userLanguage || preferredLang;
 
         // Set initial language
         setLanguage(currLang);
 
         // Listen for changes
-        $(document).on('click', '[data-set-lang]', function() {
+        $('[data-set-lang]').on('click', function () {
 
             currLang = $(this).data('setLang');
 
             if (currLang) {
-
                 setLanguage(currLang);
-
                 activateDropdown($(this));
             }
-
         });
-
 
         function setLanguage(currLang) {
             TAPi18n.setLanguage(currLang);
@@ -38,11 +35,10 @@
         function activateDropdown(elem) {
             var menu = elem.parents('.dropdown-menu');
             if (menu.length) {
-                var toggle = menu.parent().children('button, a').first();
+                var toggle = menu.prev('button, a');
                 toggle.text(elem.text());
             }
         }
-
-    } //initTranslation
+    });
 
 })(window, document, window.jQuery);
