7d0d6afe53f653d7c15d1294d3f6a1fd7bb65636
mailparajg@gmail.com <mailparajg@gmail.com>
2017-01-31 10:42:02 -0200

    Implementado para trazer informações dos jogos, players e duração da partida. A consulta precisa ser melhorada para não passar tantas vezes no each (provavelmente melhorar no aggregation)

--- imports/ui/pages/mach/match.js
+++ imports/ui/pages/mach/match.js
@@ -23,61 +23,29 @@ Template.matches.events({
 Template.matches.onCreated(function () {
     Meteor.call('matchs.findByUserAndGameDocs', Meteor.user()._id, function (e, result) {
         var retorno = [];
-        console.log('assdf');
-        console.log(result);
-        // _.map(result, function (value, key) {
-            var obj;
 
-            //Adiciona dados do jogo no array
-            // Meteor.call('game.findOne', value.game, function (eGame, resultGame) {
-            //     console.log('cccccc');
-            //     // console.log(value.timer);
-            //
-            //     var objPlayers;
-            //     _.map(value.players, function (playersValue, playersKey) {
-            //         Meteor.users.findOne({});
-            //         objPlayers.push(obj);
-            //
-            //
-            //         obj = {
-            //             _id: value._id,
-            //             players: value.players,
-            //             timer: value.timer,
-            //             place: value.place,
-            //             game: resultGame[0],
-            //             numeroParticipantes: value.players.length
-            //
-            //         };
-            //
-            //         retorno.push(obj);
-            //     })
-            // });
+        _.map(result, function (value, key) {
 
+            // @TODO melhorar essa pancada de consultas que são feitas aqui
+            var objPlayers = [];
+            _.map(value.players, function (playerValue, playerKey) {
+                objPlayers.push(Meteor.users.findOne({_id: playerValue.user_id}));
+            });
 
+            obj = {
+                _id: value._id,
+                players: value.players,
+                timer: value.timer,
+                place: value.place,
+                game: value.games_docs[0],
+                numeroParticipantes: value.players.length,
+                user_docs: objPlayers
 
+            };
+            retorno.push(obj);
+        });
 
-
-
-
-
-
-            //Adiciona dados dos usuario no array
-            // Meteor.call('game.findOne', value.game, function (eGame, resultGame) {
-            //     console.log('cccccc');
-            //     // console.log(value.timer);
-            //     obj = {
-            //         _id: value._id,
-            //         players: value.players,
-            //         timer: value.timer,
-            //         place: value.place,
-            //         game: resultGame[0],
-            //         participantes: value.players.length
-            //
-            //     };
-            //     retorno.push(obj);
-            // });
-        // });
-
+        console.log(retorno);
         rMatch.set(retorno);
     });
 
